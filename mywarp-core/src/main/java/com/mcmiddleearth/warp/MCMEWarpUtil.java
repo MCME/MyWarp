ca01ccefe23b62a7bb6011aa979917388fa18192
Eriol_Eandur <Eriol_Eandur@gmx.de>
2017-07-02 00:54:50 +0200

    Added world configuration to /warp random command Private warps are now hidden from dynmap

--- mywarp-core/src/main/java/com/mcmiddleearth/warp/MCMEWarpUtil.java
+++ mywarp-core/src/main/java/com/mcmiddleearth/warp/MCMEWarpUtil.java
@@ -17,11 +17,22 @@
 
 package com.mcmiddleearth.warp;
 
+import io.github.mywarp.mywarp.platform.LocalWorld;
+import io.github.mywarp.mywarp.platform.Platform;
 import io.github.mywarp.mywarp.warp.Warp;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -29,10 +40,53 @@ import java.util.List;
  */
 public class MCMEWarpUtil {
     
+    private static List<String> randomWarpWorlds = new ArrayList<>();
+    
+    private static Platform platform;
+    
+    static boolean init = false;
+    
+    public static void init(Platform platf) {
+        platform = platf;
+        File dataFolder = platform.getDataFolder();
+        if(dataFolder.exists()) {
+            File config = new File(dataFolder, "randomWarpConfig.txt");
+            if(!config.exists()) {
+                try {
+                    config.createNewFile();
+                    if(config.exists() && config.canWrite()) {
+                        try(PrintWriter fw = new PrintWriter(new FileWriter(config))) {
+                            fw.println("world");
+                            fw.println("moria");
+                            fw.close();
+                        }
+                    }
+                } catch (IOException ex) {
+                    Logger.getLogger(MCMEWarpUtil.class.getName()).log(Level.WARNING, null, ex);
+                }
+                
+            } else if(config.canRead()) {
+                try(Scanner scanner = new Scanner(config)) {
+                    while(scanner.hasNext()) {
+                        randomWarpWorlds.add(scanner.nextLine());
+                    }
+                } catch (FileNotFoundException ex) {
+                    Logger.getLogger(MCMEWarpUtil.class.getName()).log(Level.WARNING, null, ex);
+                }
+            }
+        }
+        init = true;
+        Logger.getGlobal().info("MCMEWarpUtil initialized!");
+    }
+    
     public static Warp getRandomWarp(Collection<Warp> warps) {
+        if(!init) {
+            return null;
+        }
         List<Warp> valid = new ArrayList<>();
         for(Warp warp: warps) {
-            if(Character.isUpperCase(warp.getName().charAt(0))) {
+            if(Character.isUpperCase(warp.getName().charAt(0))
+                    && isRandomWarpWorld(warp)) {
                 valid.add(warp);
             }
         }
@@ -42,5 +96,15 @@ public class MCMEWarpUtil {
         } else {
             return null;
         }
+    }
+    
+    private static boolean isRandomWarpWorld(Warp warp) {
+        for(String worldName: randomWarpWorlds) {
+            Optional<LocalWorld> localeWorld = platform.getGame().getWorld(worldName);
+            if(localeWorld.isPresent() && warp.getWorldIdentifier().equals(localeWorld.get().getUniqueId())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
